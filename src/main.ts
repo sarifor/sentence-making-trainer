717ecf32b844abbd302dc553d7e1334512511130
sarifor <iread1q84@gmail.com>
2022-04-03 20:34:18 +0900

    Try to show DB list on Pug

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setBaseViewsDir(join(process.cwd(), '/src/views'));
+  app.setViewEngine('pug');
   await app.listen(4000);
 }
 bootstrap();
--- src/records/records.controller.ts
+++ src/records/records.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Redirect, Post, Body } from '@nestjs/common';
+import { Controller, Get, Render, Param, Redirect, Post, Body } from '@nestjs/common';
 import { RecordsService } from './records.service';
 import { Record } from './entities/record.entity';
 
@@ -7,7 +7,17 @@ export class RecordsController {
     constructor(private readonly recordsService: RecordsService) {};
 
     @Get()
+    @Render('home')
     showAllRecords(): Record[] {
-        return this.recordsService.showAllRecords();
+        const records: Record[] = [ // for test
+            {
+                sentence: "test1",
+                translated: "테스트1",
+                source: "https://yahoo.co.jp",
+                uploaded: new Date(),
+            },            
+        ]
+        return records;
+        // return this.recordsService.showAllRecords();
     };
 };
