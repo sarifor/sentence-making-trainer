602d6663a111665582b08a4f81a7bc16760c79ca
sarifor <iread1q84@gmail.com>
2022-04-11 17:03:55 +0900

    Improve CSS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(process.cwd(), '/dist/css'));
+  app.useStaticAssets(join(process.cwd(), '/dist/css')); // Path: http://localhost:4000/styles.css
   app.setBaseViewsDir(join(process.cwd(), '/src/views'));
   app.setViewEngine('pug');
   await app.listen(4000);
