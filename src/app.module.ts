415d8fec8b6682a5425e7cd5659b4da7ef636322
sarifor <iread1q84@gmail.com>
2022-06-14 21:32:03 +0900

    Use DB with dotenv variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,20 @@ import { AppService } from './app.service';
 import { RecordsModule } from './records/records.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Record } from './records/entities/record.entity';
+// import 'dotenv/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     RecordsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: process.env.DB_PORT,
+      port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PW,
       database: process.env.DB_DBNAME,
